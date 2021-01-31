e14bc5edda69cf4f1697d01a16406e6a924b3967
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-01-31 09:52:37 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory'); 
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
